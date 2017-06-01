584f10e964f61af3e5f5332e6c55b7a787abf6f3
Jong Banaag <aabanaag@gmail.com>
2017-06-02 02:56:39 +0800

    add bg

--- src/layouts/main.js
+++ src/layouts/main.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Header from '../components/Header';
+//import Header from '../components/Header';
 
 const MainLayout = (props) => (
   <div className="main-layout container-fluid">
-    <Header title="Video City" />
     <div className="main-content">
       {props.children}
     </div>
