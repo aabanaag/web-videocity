48b36a5ada57f552f80de77d0cdef11ca8e81c64
Jong Banaag <aabanaag@gmail.com>
2017-06-04 14:06:43 +0800

    [wip] add login

--- src/client.js
+++ src/client.js
@@ -2,6 +2,7 @@ import feathers from 'feathers/client';
 import socketio from 'feathers-socketio/client';
 import hooks from 'feathers-hooks';
 import io from 'socket.io-client/dist/socket.io';
+import authentication from 'feathers-authentication-client';
 
 const socket = io('http://localhost:3030', {
   transports: ['websocket'],
@@ -9,6 +10,7 @@ const socket = io('http://localhost:3030', {
 
 const feathersClient = feathers()
   .configure(socketio(socket))
-  .configure(hooks);
+  .configure(hooks)
+  .configure(authentication({storage: localStorage}));
 
 export default feathersClient;
